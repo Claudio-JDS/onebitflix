972eb7593f54cda93c0387cb0aa26fd334c26788
Claudio Jose <claudio.jose.sousaa@gmail.com>
2024-03-05 22:56:50 -0300

    Criando a primeira rota

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import { database } from './database'
 import { adminJs, adminJsRouter } from './adminjs'
+import { router } from './routes'
 
 const app = express()
 
 app.use(express.static('public'))
 
 app.use(adminJs.options.rootPath, adminJsRouter)
+
+app.use(router)
 
 //Caso não tenha a porta env.Port vai usar a porta 3000
 const PORT = process.env.port || 3000
