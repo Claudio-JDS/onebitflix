2fc689698322c5fcf8d5c9e797f37e997f79d540
Claudio Jose <claudio.jose.sousaa@gmail.com>
2024-03-08 23:24:50 -0300

    Adicionando e removendo um gostei

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,12 +2,14 @@ import { Category } from './Category'
 import { Course } from './Course'
 import { Episode } from './Episode'
 import { Favorite } from './Favorite'
+import { Like } from './Like'
 import { User } from './User'
 
 Category.hasMany(Course, {as: 'courses'})
 
 Course.belongsTo(Category)
 Course.belongsToMany(User, {through: Favorite})
+Course.belongsToMany(User, { through: Like })//*
 Course.hasMany(Episode, { as: 'episodes' })
 Course.hasMany(Favorite, {as:'FavoritesUsers', foreignKey: 'couse_id'})
 
@@ -17,7 +19,10 @@ Favorite.belongsTo(Course)
 Favorite.belongsTo(User)
 
 User.belongsToMany(Course, {through: Favorite})
+User.belongsToMany(Course, { through: Like })//*
 User.hasMany(Favorite, {as:'FavoritesCouses', foreignKey: 'user_id'})
+
+
 
 export {
   Course,
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import express from 'express'
+import { authController } from './controllers/authController'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/courseController'
 import { episodesController } from './controllers/episodesController'
-import { authController } from './controllers/authController'
-import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth'
 import { favoritesController } from './controllers/favoritesController'
+import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth'
+import { likesController } from './controllers/likesController'
 
 const router = express.Router()
 
@@ -24,5 +25,8 @@ router.get('/episodes/stream', ensureAuthViaQuery,episodesController.stream)
 router.get('/favorites', ensureAuth, favoritesController.index)
 router.post('/favorites', ensureAuth, favoritesController.save)
 router.delete('/favorites', ensureAuth, favoritesController.delete)
+
+router.post('/likes', ensureAuth, likesController.save)
+router.delete('/likes', ensureAuth, likesController.delete)
 
 export { router }
